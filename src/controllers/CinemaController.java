c6c3d2f67f8d4052e741c9517a7ba849dc08748b
Samuel Eduardo <samueleduardorocha@gmail.com>
2024-10-18 21:33:08 -0300

    Adicionado metodo excluir Cinema

--- src/controllers/CinemaController.java
+++ src/controllers/CinemaController.java
@@ -14,39 +14,60 @@ import java.sql.SQLException;
 import java.util.LinkedList;
 
 public class CinemaController {
-    public static boolean inserirCinema(Cinema cinema){
+    public static boolean inserirCinema(Cinema cinema) {
         String sql = "INSERT INTO cinema (id_cinema, nome_cinema, id_endereco) VALUES (?, ?, ?)";
 
         try (Connection conn = Database.conectar();
-            PreparedStatement pstmt = conn.prepareStatement(sql)){
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             int idCinema = CinemaController.getMaiorId();
-            if(idCinema == -500){
+            if (idCinema == -500) {
                 idCinema = 0;
-            } else if(idCinema == -999){
+            } else if (idCinema == -999) {
                 return false;
             }
 
-            pstmt.setInt(1, idCinema+1);
+            pstmt.setInt(1, idCinema + 1);
             pstmt.setString(2, cinema.getNome());
             pstmt.setInt(3, cinema.getEndereco().getIdEndereco());
 
             pstmt.executeUpdate();
 
-            return true; 
-            
+            return true;
+
         } catch (Exception e) {
             System.err.println("Erro ao inserir cinema: " + e.getMessage());
             return false;
         }
     }
 
-    public static boolean existeCinema (int idCinema) {
+    public static boolean excluirCinema(int idCinema) {
+        String sql = "DELETE FROM cinema WHERE id_cinema = ?";
+
+        if (CinemaController.existeCinema(idCinema)) {
+            try (Connection conn = Database.conectar();
+                    PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+                pstmt.setInt(1, idCinema);
+                pstmt.executeUpdate();
+                return true;
+
+            } catch (SQLException e) {
+                MenuFormatter.msgTerminalERROR(e.getMessage());
+                return false;
+            }
+        } else {
+            MenuFormatter.msgTerminalERROR("Cinema não encontrado no Banco de Dados.");
+            return false;
+        }
+    }
+
+    public static boolean existeCinema(int idCinema) {
         String sql = "SELECT COUNT(id_cinema) AS resultado FROM cinema WHERE id_cinema = ?;";
         int qtdCinema = 0;
 
         try (Connection conn = Database.conectar();
-            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             pstmt.setInt(1, idCinema);
             ResultSet rs = pstmt.executeQuery();
@@ -57,7 +78,7 @@ public class CinemaController {
 
             if (qtdCinema == 0) {
                 return false;
-                
+
             } else {
                 return true;
             }
@@ -73,7 +94,7 @@ public class CinemaController {
         int ultimoId = -500;
 
         try (Connection conn = Database.conectar();
-            Statement pstmt = conn.createStatement()) {
+                Statement pstmt = conn.createStatement()) {
 
             ResultSet rs = pstmt.executeQuery(sql);
             while (rs.next()) {
